f8a43fa9c9a1c1db297a97faae7c09305c2367b3
mahesh <udayipmakri@gmail.com>
2023-09-15 21:22:23 +0530

    object of snake and random apple is completed

--- frame.java
+++ frame.java
@@ -13,49 +13,37 @@ public frame()
         setBounds(400, 50, 600, 600);
         
         
-         JPanel snakPanel =new JPanel(){
-        
+         JPanel snakPanel =new JPanel(){   
+
+
            protected void paintComponent(Graphics g) {
            super.paintComponent(g);
 
            int x=1;
            int n=3;
-            
+           Random rx =new Random();
+           Random ry =new Random(); 
+           int X_apple=rx.nextInt(getWidth()-50);
+           int Y_apple=ry.nextInt(getWidth()-60);
+
+
+           
            for(int i=0;i<n;i++){
            g.setColor(Color.green);
            g.fillOval(100,100+x,20, 20); 
            x=x+17;
            }
+         
+
+            g.setColor(Color.red);
+            g.fillOval(X_apple,Y_apple,20, 20); 
+                    
         }
         
         };  
            snakPanel.setBackground(Color.BLACK);                    
             add(snakPanel);
-            setVisible(true);    
-
-        
-         
-
-          JPanel apple = new JPanel() {
-                @Override
-                protected void paintComponent(Graphics g) {
-                     super.paintComponent(g);
-
-                     Random rx =new Random();
-                     Random ry =new Random();
-
-                     int x=rx.nextInt(getWidth()-50);
-                     int y=ry.nextInt(getWidth()-60);
-
-                    g.setColor(Color.red);
-                    g.fillOval(x,y,20, 20); 
-                    
-                    
-                }
-            };
-            apple.setBackground(Color.BLACK);                    
-            add(apple);
-            setVisible(true);  
+            setVisible(true); 
 
           
          
